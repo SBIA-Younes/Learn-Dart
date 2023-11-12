a71d3c29bf97235b8e8fd1dc9c13def2aa86e43c
SBIA-Younes <brahim-sb@hotmail.fr>
2023-11-12 14:21:19 +0100

    add a print for my variable

--- Nullable variables.dart
+++ Nullable variables.dart
@@ -2,7 +2,8 @@
 void main() {
   // !!  int a = null; => FR: ce code revoie une erreur, Une variable de type int ne peux pas avoir la valeur null
 
-  int? a = null; // ? FR: Lors de la création d'une variable, ajouter  " ? "  au type pour indiquer que la variable peut étre null
+  int? a =
+      null; // ? FR: Lors de la création d'une variable, ajouter  " ? "  au type pour indiquer que la variable peut étre null
 
   // ? FR : Vous pouvez simplefier un peu ce code car, dans toutes les variables de Dart, null est la valeur par default pour les variables non initialisées
 
@@ -20,4 +21,6 @@ void main() {
 
   String? nam = "Jane";
   String? address = null;
+
+  print('$a, $b, $nam, $address');
 }
