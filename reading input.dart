15212dc03285c17e8c8bf1425805dc2664db5b9e
SBIA-Younes <brahim-sb@hotmail.fr>
2023-11-14 11:03:04 +0100

    add a condition and structure printing in terminal

--- reading input.dart
+++ reading input.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  print('Entre name :');
+  // print('Entre name :');
+  stdout.write('Entre name : ');
   String? name = stdin.readLineSync();
-  print('name : ${name ?? "Anonyme"}');
+  print('name : ${name}');
 
-  print('Entre yeur age :');
+  // print('Entre yeur age :');
+  stdout.write('Entre your age : ');
   String inputAge = stdin.readLineSync() ?? '';
   int age = 0;
   if (int.tryParse(inputAge) != null) {
@@ -14,8 +16,10 @@ void main(List<String> args) {
     print("Error");
   }
 
-  print('Entre yeur salery :');
+  // print('Entre yeur salery :');
+  stdout.write('Entre your salery : ');
   double? salery = double.tryParse(stdin.readLineSync() ?? '');
 
   print('name : $name, age : $age, salery : ${salery ?? 0.0}');
 }
+// comment install snap dans ubuntu ?
